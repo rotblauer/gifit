9537bbf869949f4cf34946e6a3698c5908728048
Mr. Is <isaac.ardis@gmail.com>
2016-12-21 12:21:17 -0500

    ![flags](http://media0.giphy.com/media/3o7TKv7S69WRS6awc8/giphy-tumblr.gif)

--- gifit.go
+++ gifit.go
@@ -19,8 +19,9 @@ type GiphyQueryResponse struct {
 	Pagination ResPagination `json: "pagination"`
 }
 type Gif struct {
-	ID   string `json: "id"`
-	Slug string `json: "slug"`
+	ID          string `json: "id"`
+	Slug        string `json: "slug"`
+	BitlyGifUrl string `json: "bitly_gif_url"`
 	// there's a lot more... TODO?
 	Images ImagesObj `json: "images"`
 }
@@ -60,43 +61,88 @@ func formatMarkdownImageMarkup(altText, source string) string {
 }
 
 func main() {
+
+	useMarkdown := true
+	useEmbeddable := false
+	copyToClipboard := false
+
+	var out string
+
+	// flag.BoolVar(&useMarkdown, "m", false, "format as markdown")
+	// flag.BoolVar(&useEmbeddable, "e", false, "use embeddable")
+	// flag.BoolVar(&copyToClipboard, "p", false, "copy to clipboard")
+
+	// flag.Parse()
+
+	// if !useEmbeddable {
+	// 	useMarkdown = true
+	// 	fmt.Println("Using embed.")
+	// } else {
+	// 	fmt.Println("Using markdown.")
+	// }
+
 	// $@
 	args := os.Args[1:]
 	if len(args) == 0 {
 		fmt.Println("Useage: $ gifit hello kitty")
+		// fmt.Println("-m : format as markdown  | cannot use with -e | DEFAULT")
+		// fmt.Println("-e : format as embed url | cannot use with -m")
+		// fmt.Println("-p : copy to clipboard (pbcopy)")
 		return
 	}
-	commit_message := strings.Join(args, " ")
-	encoded_query := url.QueryEscape(commit_message)
+	// Remove all args that are flags.
+	var queryArgs []string
+	for _, arg := range args {
+		if !strings.HasPrefix(arg, "-") {
+			queryArgs = append(queryArgs, arg)
+		}
+	}
+	queryArgsString := strings.Join(queryArgs, " ")
+	encodedQuery := url.QueryEscape(queryArgsString)
 
 	res := GiphyQueryResponse{}
-	getJson("http://api.giphy.com/v1/gifs/search?q="+encoded_query+"&limit=1&api_key="+GiphyPublicAPIKey, &res)
+	getJson("http://api.giphy.com/v1/gifs/search?q="+encodedQuery+"&limit=1&api_key="+GiphyPublicAPIKey, &res)
 
 	if len(res.Data) == 0 {
 		fmt.Println("Got 0 results for the GIF search. Shiiii. Try again?")
 		return
 	}
 
-	// gif_id := res.Data[0].ID
-	// markdownImageSource := "http://i.giphy.com/" + gif_id + ".gif"
-	// TODO: handle different kinds of file url provided in Gif json object (?) so we don't have to hardcode url?
-	// doing....
 	markdownImageSource := res.Data[0].Images.Downsized.Url
+	embeddableUrl := res.Data[0].BitlyGifUrl
 
-	ga := exec.Command("git", "add", "-A")
-	ga_out, ga_err := ga.Output()
-	if ga_err != nil {
-		fmt.Println(ga_err)
-		return
+	if useMarkdown {
+		out = formatMarkdownImageMarkup(queryArgsString, markdownImageSource)
 	}
-	fmt.Println(string(ga_out))
+	if useEmbeddable {
+		out = embeddableUrl
+	}
+	if copyToClipboard {
+		c := exec.Command("pbcopy", out)
+		err := c.Run()
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		fmt.Printf("(Copied to clipboard.) %s", out)
+	} else {
+		fmt.Printf("%s", out)
+	}
 
-	gc := exec.Command("git", "commit", "-m", "'"+formatMarkdownImageMarkup(commit_message, markdownImageSource)+"'")
-	gc_out, gc_err := gc.Output()
-	if gc_err != nil {
-		fmt.Println(gc_err)
-		return
-	}
-	fmt.Println(string(gc_out))
+	// ga := exec.Command("git", "add", "-A")
+	// ga_out, ga_err := ga.Output()
+	// if ga_err != nil {
+	// 	fmt.Println(ga_err)
+	// 	return
+	// }
+	// fmt.Println(string(ga_out))
+
+	// gc := exec.Command("git", "commit", "-m", "'"+formatMarkdownImageMarkup(commit_message, markdownImageSource)+"'")
+	// gc_out, gc_err := gc.Output()
+	// if gc_err != nil {
+	// 	fmt.Println(gc_err)
+	// 	return
+	// }
+	// fmt.Println(string(gc_out))
 
 }
